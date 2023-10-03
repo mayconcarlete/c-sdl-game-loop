ed480a5512702616fec5c0ae16901f7fa397467e
Maycon Carlete <maycon.carlete@gmail.com>
2023-10-03 08:18:15 -0400

    feat sdl configuration

---
+++ include/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WINDOW_TITLE "SDL Game Loop"
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include "SDL2/SDL.h"
+#include "config.h"
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+bool initialize_window(void){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+    window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if(!window){
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        0
+    );
+    if(!renderer){
+        fprintf(stderr, "Error creating SDL Renderer.\n");
+    }
+    return true;
+}
+
 
 int main(){
-    SDL_Init(SDL_INIT_EVERYTHING);
-    printf("Hello from peskland");
+    bool initialize_is_success = initialize_window();
+    if(initialize_is_success == false){
+        return -1;
+    }
+    printf("Hello from peskland\n");
     return 0;
 }
